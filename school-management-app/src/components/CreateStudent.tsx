d4e03b6fbefffe9c01082f7cc3c3484ba243c4ab
Rogelio Gamboa Jr <roggam@yahoo.com.mx>
2022-02-23 11:54:05 -0500

    created Form to create new students

--- school-management-app/src/components/CreateStudent.tsx
+++ school-management-app/src/components/CreateStudent.tsx
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
-import { Modal, ModalBody, ModalHeader } from "reactstrap";
+import {
+  Button,
+  Col,
+  Form,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Row,
+} from "reactstrap";
 
 type MyProps = {
   isOpen: boolean;
@@ -11,10 +19,45 @@ export class CreateStudent extends Component<MyProps> {
   render() {
     return (
       <div>
-        <Modal isOpen = {this.props.isOpen} toggle = {this.props.toggle}>
-          <ModalHeader toggle = {this.props.toggle}>
-              Adding New Student
+        <Modal isOpen={this.props.isOpen} toggle={this.props.toggle}>
+          <ModalHeader toggle={this.props.toggle}>
+            Adding New Student
           </ModalHeader>
+          <Form>
+            <Row>
+              <Col>
+                <label>First Name </label>
+              </Col>
+              <Col>
+                <input id="firstName" name="firstName" type="text"></input>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <label>Last Name </label>
+              </Col>
+              <Col>
+                <input id="lastName" name="lastName" type="text"></input>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <label>Age </label>
+              </Col>
+              <Col>
+                <input id="age" name="age" type="number"></input>
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                <label>Teacher </label>
+              </Col>
+              <Col>
+                <input id="teacher" name="teacher" type="text"></input>
+              </Col>
+            </Row>
+            <Button color="primary">Create New Student</Button>
+          </Form>
           <ModalBody></ModalBody>
         </Modal>
       </div>
