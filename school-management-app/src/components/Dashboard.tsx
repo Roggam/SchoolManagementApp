0fefbb837d9a5a8f4b5e5fb0bf0af0650c989fac
Rogelio Gamboa Jr <roggam@yahoo.com.mx>
2022-02-23 10:48:19 -0500

    rendering student names

--- school-management-app/src/components/Dashboard.tsx
+++ school-management-app/src/components/Dashboard.tsx
@@ -20,19 +20,22 @@ import {
 import axios from "axios";
 
 interface MyState {
-    students: []
+  students: [];
 }
 
 export class Dashboard extends Component<{}, MyState> {
   state: MyState = {
-      students: []
+    students: [],
+  };
+
+  componentDidMount() {
+    axios.get("http://localhost:8080/list").then((res) => {
+      const students = res.data;
+      this.setState({ students });
+    });
   }
-  
-  componentDidMount (){
-      axios.get('https://localhost:8080/list')
-  }
-  
-    render() {
+
+  render() {
     return (
       <div>
         <Navbar color="dark" light mb-2>
@@ -51,31 +54,32 @@ export class Dashboard extends Component<{}, MyState> {
                   <IoMan className="font-size-xl" /> 86 Students
                 </CardTitle>
                 <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </CardText>
                 <Button>Manage Students</Button>
               </Card>
             </Col>
             <Col sm="4">
-            <Card body>
+              <Card body>
                 <CardTitle tag="h5">
                   <IoSchoolOutline className="font-size-xl" /> 20 Teachers
                 </CardTitle>
-                <CardText>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    </CardText>
+                <CardText>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </CardText>
                 <Button>Manage Teachers</Button>
               </Card>
             </Col>
             <Col sm="4">
-            <Card body>
+              <Card body>
                 <CardTitle tag="h5">
                   <IoSettings className="font-size-xl" /> 13 Employees
                 </CardTitle>
                 <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </CardText>
                 <Button>Manage Employees</Button>
               </Card>
@@ -83,16 +87,34 @@ export class Dashboard extends Component<{}, MyState> {
           </Row>
         </Container>
         <Container className="mt-4">
-            <Row>
-                <Col sm='12'>
-                     <Button block color="success">
-                         <span className="font-size-l">Create New Student</span>
-                     </Button>
-                </Col>
-            </Row>
-           
+          <Row>
+            <Col sm="12">
+              <Button block color="success">
+                <span className="font-size-l">Create New Student</span>
+              </Button>
+            </Col>
+          </Row>
+        </Container>
+
+        <Container className="mt-4">
+          {this.state.students.map((student) => renderStudent(student))}
         </Container>
       </div>
     );
   }
+}
+function renderStudent(st) {
+  return (
+    <Row>
+      <Col sm="12">
+        <Card body> 
+        <CardTitle>
+          <IoMan className="font-size-xl"/> 
+          {st.firstName + " " + st.lastName}
+        </CardTitle>
+
+        </Card>
+      </Col>
+    </Row>
+  );
 }
